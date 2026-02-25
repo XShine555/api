2426308ab4f5e5517a495a2c190d1a236fd9614c
NotS1mp <xshine555@gmail.com>
2026-02-25 18:53:47 +0100

    Fix

--- src/main/java/com/musify/controllers/PlaylistController.java
+++ src/main/java/com/musify/controllers/PlaylistController.java
@@ -12,10 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.musify.DTOs.Playlist.PlaylistResponseDTO;
@@ -110,8 +111,10 @@ public class PlaylistController {
 
     @PutMapping("/{id}")
     public ResponseEntity<PreviewPlaylistResponseDTO> updatePlaylist(@PathVariable Long id,
-            @RequestBody PlaylistUpdateDTO dto) {
+            @RequestPart("title") String title,
+            @RequestPart(value = "image", required = false) MultipartFile image) {
         try {
+            PlaylistUpdateDTO dto = new PlaylistUpdateDTO(title, image);
             return playlistService.updatePlaylist(id, dto)
                     .map(this::toPreviewResponseDTO)
                     .map(ResponseEntity::ok)

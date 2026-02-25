98e79cd67bb8dc6157176ca4c111940a01a81fc3
NotS1mp <xshine555@gmail.com>
2026-02-25 19:14:00 +0100

    api

--- src/main/java/com/musify/config/SecurityConfig.java
+++ src/main/java/com/musify/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/images/**").permitAll()
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/images/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider(userDetailsService, passwordEncoder))
